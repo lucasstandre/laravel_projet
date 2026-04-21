524f78971596ebd77277c463749cb505437391b1
lucasstandre <146019884+lucasstandre@users.noreply.github.com>
2026-04-20 22:40:01 -0400

    blade de filtre + search...
    blade de filtre + recherche, le css reste a changer mais le fonctionnement est la pour quon puisse le réutiliser

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -11,13 +11,26 @@ class PlaylistController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() : View
+    public function index(Request $request) : View
     {
+        $query = Playlist::query(); // contructeur de requete pour commencer une query
+        // si ya une request qui est search
+        if ($request->has('search')) {
+        $query->where('playlist', 'like', '%' . $request->search . '%'); // ont fait un where playlist like %rock% donc on peut etre au millieu dun mot
+        }
+        // si le filtre est fait donc on check si yer original (un bool) qui dit si la playlist est fait par le user (example de filtre)
+        if ($request->filled('original')) {
+            $query->where('original', $request->original);
+        }
+
+        $playlists = $query->get();
+
         return view('playlist/playlists', [
         // D’autres paramètres peuvent être passés à la vue en les séparant par une virgule.
-        'playlists' => Playlist::All()
+        'playlists' => $playlists // retourn toute les playlists apres la requete, par default sa pogne all
         ]);
     }
+
 
     /**
      * Show the form for creating a new resource.
